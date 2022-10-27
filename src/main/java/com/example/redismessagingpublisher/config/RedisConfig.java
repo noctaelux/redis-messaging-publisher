1dbc3b9501ae58387c1315bb110fdf216725d1c8
Oscar Calzada <noctaelux@gmail.com>
2022-10-27 09:34:20 -0500

    fix imports

--- src/main/java/com/example/redismessagingpublisher/config/RedisConfig.java
+++ src/main/java/com/example/redismessagingpublisher/config/RedisConfig.java
@@ -9,7 +9,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
 public class RedisConfig {
