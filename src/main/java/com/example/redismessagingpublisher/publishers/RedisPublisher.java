d4088d8950926a06111c200df014c32bc6cf654c
Oscar Calzada <noctaelux@gmail.com>
2022-12-13 21:09:16 -0600

    Se aplican fixes post testing

--- src/main/java/com/example/redismessagingpublisher/publishers/RedisPublisher.java
+++ src/main/java/com/example/redismessagingpublisher/publishers/RedisPublisher.java
@@ -1,6 +1,8 @@
 package com.example.redismessagingpublisher.publishers;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RedisPublisher implements Publisher{
 
+    private final Logger LOG = LoggerFactory.getLogger(RedisPublisher.class);
+
     private final RedisTemplate<String,Object> redisTemplate;
 
     private final ChannelTopic channelTopic;
@@ -16,5 +20,7 @@ public class RedisPublisher implements Publisher{
     @Override
     public void publish(Object message) {
         redisTemplate.convertAndSend(channelTopic.getTopic(),message);
+        LOG.info("TOPIC: {}", channelTopic.getTopic());
+        LOG.info("MESSAGE: {}", message);
     }
 }
